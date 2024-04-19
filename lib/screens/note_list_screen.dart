c1ccf73450eedd3b362fd78b88c5f422bbf480c9
fariskyzaky <119941340+fariskyzaky@users.noreply.github.com>
2024-04-19 10:22:00 +0700

    Update note_list_screen.dart

--- lib/screens/note_list_screen.dart
+++ lib/screens/note_list_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -79,6 +77,8 @@ class _NoteListScreenState extends State<NoteListScreen> {
                             Navigator.of(context).pop();
                           },
                         );
+                        _titleController.clear();
+                        _descriptionController.clear();
                       },
                       child: const Text('Save')),
                 ],
@@ -94,43 +94,152 @@ class _NoteListScreenState extends State<NoteListScreen> {
 }
 
 class NoteList extends StatelessWidget {
+
   const NoteList({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController titleController = TextEditingController();
+    TextEditingController descriptionController = TextEditingController();
+
     return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('notes').snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return new Text('Error: ${snapshot.error}');
-        }
-        return ListView(
-          padding: const EdgeInsets.only(bottom: 80),
-          children: snapshot.data!.docs.map((document) {
-            return Card(
-              child: ListTile(
-                title: Text(document['title']),
-                subtitle: Text(document['description']),
-                trailing: InkWell(
-                  onTap: () {
-                    FirebaseFirestore.instance
-                        .collection('notes')
-                        .doc(document.id)
-                        .delete()
-                        .catchError((e) {
-                      print(e);
-                    });
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    child: const Icon(Icons.delete),
-                  ),
-                ),
-              ),
-            );
-          }).toList()
-        );
-      }
-    );
+        stream: FirebaseFirestore.instance.collection('notes').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return new Text('Error: ${snapshot.error}');
+          }
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            default:
+              return ListView(
+                  padding: const EdgeInsets.only(bottom: 80),
+                  children: snapshot.data!.docs.map((document) {
+                    return Card(
+                      child: ListTile(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              TextEditingController titleController = TextEditingController(text: document['title']);
+                              TextEditingController descriptionController = TextEditingController(text: document['description']);
+
+                              return AlertDialog(
+                                title: const Text('Update Notes'),
+
+                                content: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'Title: ',
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    TextField(
+                                      controller: titleController,    
+                                    ),
+                                    const Padding(
+                                      padding: const EdgeInsets.only(top: 10),
+                                      child: Text(
+                                        'Description: ',
+                                        textAlign: TextAlign.start,
+                                      ),
+                                    ),
+                                    TextField(
+                                      controller: descriptionController,
+                                    ),
+                                  ],
+                                ),
+                                actions: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal:
+                                            30), //--> Mengatur jarak antar tombol
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.of(context)
+                                            .pop(); //--> Tombol Menutup dialog atau cancel
+                                      },
+                                      child: Text('Cancel'),
+                                    ),
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () {
+                                        Map<String, dynamic> updateNote = {};
+                                        updateNote['title'] = titleController.text;
+                                        updateNote['description'] = descriptionController.text;
+
+                                        FirebaseFirestore.instance
+                                            .collection('notes')
+                                            .doc(document.id) //--> sintax agar update berdasarkan id, kalo tidak ada ini maka akan mengupdate semua
+                                            .update(updateNote)
+                                            .whenComplete(
+                                          () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        );
+                                        titleController.clear();
+                                        descriptionController.clear();
+                                      },
+                                      child: const Text('Update')),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        title: Text(document['title']),
+                        subtitle: Text(document['description']),
+                        trailing: InkWell(
+                          onTap: () {
+                            showDialog(
+                            context: context,
+                            builder: (context) {
+                               return AlertDialog(
+                              content: Column(
+                                children: [
+                                  Text('Delete Data')
+                                ],
+                              ),
+
+                            actions: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal:
+                                            30), //--> Mengatur jarak antar tombol
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(); //--> Tombol Menutup dialog atau cancel
+                                      },
+                                      child: Text('Cancel'),
+                                    ),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      FirebaseFirestore.instance
+                                        .collection('notes')
+                                        .doc(document.id)
+                                        .delete()
+                                        .catchError((e) {
+                                      print(e);
+                                    });
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Update')),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Icon(Icons.delete),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList());
+          }
+        });
   }
 }
